2ba34bb1ed727368a119f3709139f905e5acf57b
Johnathon Wood <jaywood128@gmail.com>
2019-04-21 15:16:53 -0400

    Push

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :authors, only: [:show, :index] do
+    resources :posts, only: [:show, :index, :new, :edit]
+  end
+  resources :posts, only: [:show, :index, :update]
+
+  post 'posts', to: 'posts#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
